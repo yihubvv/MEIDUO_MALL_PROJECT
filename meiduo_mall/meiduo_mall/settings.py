ce219f2e0318671ea1bf42b3ac239045cf741545
ccy <2074441723@qq.com>
2026-05-24 12:53:28 -0400

    updated setting

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.users.apps.UsersConfig',
+    'apps.users',
     # CORS
     'corsheaders',
 ]
