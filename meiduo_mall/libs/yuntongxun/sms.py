aedef3346716927eaa3dab909f8879409a517266
ccy <2074441723@qq.com>
2026-05-25 20:23:00 -0400

    added messagee

--- meiduo_mall/libs/yuntongxun/sms.py
+++ meiduo_mall/libs/yuntongxun/sms.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from .CCPRestSDK import REST
+from libs.yuntongxun.CCPRestSDK import REST
 
 # 说明：主账号，登陆云通讯网站后，可在"控制台-应用"中看到开发者主账号ACCOUNT SID
 _accountSid = '8aaf0708568d4143015697b0f4960888'
