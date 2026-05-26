a9c85c59f1535e73de44cf6840825c98a215ad61
ccy <2074441723@qq.com>
2026-05-26 17:25:03 -0400

    changed host

--- front_end_pc/js/host.js
+++ front_end_pc/js/host.js
@@ -1,3 +1,4 @@
-// 保存后端API服务器地址
- var host = window.location.protocol + '//' + window.location.hostname + ':8000';
+// Store the backend API server address
+
+var host = window.location.protocol + '//' + window.location.hostname + ':8000';
 //var host = 'http://127.0.0.1:8000';
