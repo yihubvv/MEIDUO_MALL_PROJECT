1f76c39a01f5826f6e0699be9846358621d3def0
ccy <2074441723@qq.com>
2026-05-28 13:37:50 -0400

    changed user model

--- meiduo_mall/apps/areas/models.py
+++ meiduo_mall/apps/areas/models.py
@@ -1,24 +1,23 @@
 from django.db import models
 
-# Create your models here.
 
 class Area(models.Model):
-    """Administrative Region"""
-    name = models.CharField(max_length=20, verbose_name='Name')
+    """Province / city / district administrative region."""
 
+    name = models.CharField(max_length=20, verbose_name='Name')
     parent = models.ForeignKey(
         'self',
-        on_delete=models.SET_NULL,
-        related_name='subs',
         null=True,
         blank=True,
-        verbose_name='Parent Administrative Region'
+        on_delete=models.SET_NULL,
+        related_name='subs',
+        verbose_name='Parent Administrative Region',
     )
 
     class Meta:
         db_table = 'tb_areas'
         verbose_name = 'Province / City / District'
-        verbose_name_plural = 'Province / City / District'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.name
--- meiduo_mall/apps/users/models.py
+++ meiduo_mall/apps/users/models.py
@@ -19,7 +19,31 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     mobile=models.CharField(max_length=11,unique=True)
     email_active = models.BooleanField(default=False, verbose_name='Verify Email Status')
+    default_address = models.ForeignKey('Address', related_name='users', null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Default Address')
+
     class Meta:
         db_table='tb_users'
         verbose_name='User Management'
         verbose_name_plural=verbose_name
+
+
+from utils.models import BaseModel
+class Address(BaseModel):
+    """User Address"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='User')
+    title = models.CharField(max_length=20, verbose_name='Address Name')
+    receiver = models.CharField(max_length=20, verbose_name='Recipient')
+    province = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='province_addresses', verbose_name='Province')
+    city = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='city_addresses', verbose_name='City')
+    district = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='district_addresses', verbose_name='District')
+    place = models.CharField(max_length=50, verbose_name='Address')
+    mobile = models.CharField(max_length=11, verbose_name='Mobile Phone')
+    tel = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='Landline Phone')
+    email = models.CharField(max_length=30, null=True, blank=True, default='', verbose_name='Email')
+    is_deleted = models.BooleanField(default=False, verbose_name='Logical Deletion')
+
+    class Meta:
+        db_table = 'tb_address'
+        verbose_name = 'User Address'
+        verbose_name_plural = verbose_name
+        ordering = ['-update_time']
