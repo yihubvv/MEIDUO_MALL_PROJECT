30bed8ee87a0d8edcf7f9564970d5f0b2f4e1fd9
ccy <2074441723@qq.com>
2026-05-29 14:04:03 -0400

    working on storage

--- meiduo_mall/apps/contents/views.py
+++ meiduo_mall/apps/contents/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
+from fdfs_client.client import Fdfs_client
+
+
+def get_fdfs_client():
+    return Fdfs_client(settings.FDFS_CLIENT_CONF)
--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -8,6 +8,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FDFS_CLIENT_CONF = os.path.join(BASE_DIR, 'utils/fastdfs/client.conf')
 
 # ctl + K  used for git add and commit operations
 # ctl + shift + k used for git push
