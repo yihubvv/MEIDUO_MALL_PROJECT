61f7803ad50d6adec87042818338b4d88fad4539
ccy <2074441723@qq.com>
2026-05-30 16:11:43 -0400

    update

--- meiduo_mall/apps/goods/views.py
+++ meiduo_mall/apps/goods/views.py
@@ -107,3 +107,25 @@ class DetailView(View):
       'specs': goods_specs,
     }
     return render(request,'detail.html',context)
+  
+from django.template import loader
+from meiduo_mall import settings
+import os
+def generic_meiduo_index(self,request):
+    categories = get_categories()
+    contents= {}
+    content_categories = ContentCategory.objects.all()
+    for cat in content_categories:
+      contents[cat.key] = cat.content_set.filter(status=True).order_by('sequence')
+    
+    context = {
+      'categories': categories,
+      'contents': contents,
+    }
+    index_template = loader.get_template('index.html')
+    index_html_data = index_template.render(context)
+
+    file_path = os.path.join(os.path.dirname(settings.BASE_DIR),'front_end_pc/index.html')
+    with open(file_path, 'w', encoding='utf-8') as f:
+      f.write(index_html_data)
+    return render(request, 'index.html', context)
