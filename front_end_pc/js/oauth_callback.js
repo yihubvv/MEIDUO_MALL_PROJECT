57c14977d3f378d987e52588b0d6df4cb9b4bcb7
ccy <2074441723@qq.com>
2026-06-02 19:24:08 -0400

    update

--- front_end_pc/js/oauth_callback.js
+++ front_end_pc/js/oauth_callback.js
@@ -1,4 +1,4 @@
-var site_url = 'http://www.meiduo.site:8080';
+var site_url = window.location.protocol + '//' + window.location.host;
 
 var vm = new Vue({
     el: '#app',
@@ -20,12 +20,12 @@ var vm = new Vue({
         sms_code: '',
         access_token: '',
 
-        image_code:'',
-        error_image_code_message:'',
-        error_image_code:'',
-        image_code_url:''
+        image_code: '',
+        error_image_code_message: '',
+        error_image_code: '',
+        image_code_url: ''
     },
-    mounted: function(){
+    mounted: function () {
         if (window.location.hostname == 'localhost' || window.location.hostname == '127.0.0.1') {
             location.replace(site_url + window.location.pathname + window.location.search + window.location.hash);
             return;
@@ -39,10 +39,10 @@ var vm = new Vue({
         var code = this.get_query_string('code');
         axios.get(this.host + '/oauth_callback/?code=' + code, {
                 responseType: 'json',
-                withCredentials:true,
+                withCredentials: true,
             })
             .then(response => {
-                if (response.data.code == 0){
+                if (response.data.code == 0) {
                     // Connected account
                     var state = this.get_query_string('state');
                     location.href = site_url + '/';
@@ -58,15 +58,15 @@ var vm = new Vue({
             })
     },
     methods: {
-              //Generate an image captcha ID and set the src attribute of the image captcha <img> element on the page
-		generate_image_code: function(){
-// Generate an ID: For strict uniqueness, use a UUID. In less strict scenarios, a timestamp can also be used.			
+        //Generate an image captcha ID and set the src attribute of the image captcha <img> element on the page
+        generate_image_code: function () {
+            // Generate an ID: For strict uniqueness, use a UUID. In less strict scenarios, a timestamp can also be used.			
             this.image_code_id = generateUUID();
-// Set the src attribute of the image captcha <img> element on the page.
-			this.image_code_url = this.host + "/image_codes/" + this.image_code_id + "/";
-		},
-// Get URL path parameters
-            get_query_string: function(name){
+            // Set the src attribute of the image captcha <img> element on the page.
+            this.image_code_url = this.host + "/image_codes/" + this.image_code_id + "/";
+        },
+        // Get URL path parameters
+        get_query_string: function (name) {
             var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
             var r = window.location.search.substr(1).match(reg);
             if (r != null) {
@@ -74,40 +74,40 @@ var vm = new Vue({
             }
             return null;
         },
-        check_pwd: function (){
+        check_pwd: function () {
             var len = this.password.length;
-            if(len<8||len>20){
+            if (len < 8 || len > 20) {
                 this.error_password = true;
             } else {
                 this.error_password = false;
             }
         },
-        check_phone: function (){
+        check_phone: function () {
             var re = /^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/;
-            if(re.test(this.mobile)) {
+            if (re.test(this.mobile)) {
                 this.error_phone = false;
             } else {
                 this.error_phone_message = 'Bad Phone Format';
                 this.error_phone = true;
             }
         },
-        check_sms_code: function(){
-            if(!this.sms_code){
+        check_sms_code: function () {
+            if (!this.sms_code) {
                 this.error_sms_code_message = 'Please Enter SMS';
                 this.error_sms_code = true;
             } else {
                 this.error_sms_code = false;
             }
         },
-		check_image_code: function (){
-			if(!this.image_code) {
-				this.error_image_code_message = 'Pleases Enter Captcha.';
-				this.error_image_code = true;
-			} else {
-				this.error_image_code = false;
-			}
-		},
-        send_sms_code: function(){
+        check_image_code: function () {
+            if (!this.image_code) {
+                this.error_image_code_message = 'Pleases Enter Captcha.';
+                this.error_image_code = true;
+            } else {
+                this.error_image_code = false;
+            }
+        },
+        send_sms_code: function () {
             if (this.sending_flag == true) {
                 return;
             }
@@ -120,19 +120,19 @@ var vm = new Vue({
                 return;
             }
 
-// Send a request to the backend API and let the backend send the SMS verification code
-// var url = this.host + '/sms_codes/' + this.mobile + '/'
-// Send a request to the backend API and let the backend send the SMS verification code
-            var url = this.host + '/sms_codes/' + this.mobile + '/' + '?image_code=' + this.image_code
-                + '&image_code_id=' + this.image_code_id
+            // Send a request to the backend API and let the backend send the SMS verification code
+            // var url = this.host + '/sms_codes/' + this.mobile + '/'
+            // Send a request to the backend API and let the backend send the SMS verification code
+            var url = this.host + '/sms_codes/' + this.mobile + '/' + '?image_code=' + this.image_code +
+                '&image_code_id=' + this.image_code_id
             axios.get(url, {
                     responseType: 'json',
-                    withCredentials:true,
+                    withCredentials: true,
                 })
                 .then(response => {
-// Indicates that the backend successfully sent the SMS verification code
-// Start a 60-second countdown.
-// After 60 seconds, allow the user to click the "Send SMS Verification Code" button again.
+                    // Indicates that the backend successfully sent the SMS verification code
+                    // Start a 60-second countdown.
+                    // After 60 seconds, allow the user to click the "Send SMS Verification Code" button again.
                     var num = 60;
                     // Set a timer
                     var t = setInterval(() => {
@@ -160,13 +160,13 @@ var vm = new Vue({
                     this.sending_flag = false;
                 })
         },
-       // save
-        on_submit: function(){
+        // save
+        on_submit: function () {
             this.check_pwd();
             this.check_phone();
             this.check_sms_code();
 
-            if(this.error_password == false && this.error_phone == false && this.error_sms_code == false) {
+            if (this.error_password == false && this.error_phone == false && this.error_sms_code == false) {
                 axios.post(this.host + '/oauth_callback/', {
                         password: this.password,
                         mobile: this.mobile,
@@ -174,13 +174,13 @@ var vm = new Vue({
                         access_token: this.access_token
                     }, {
                         responseType: 'json',
-                        withCredentials:true,
+                        withCredentials: true,
                     })
                     .then(response => {
-// Record the user's login status
+                        // Record the user's login status
                         location.href = site_url + '/'
                     })
-                    .catch(error=> {
+                    .catch(error => {
                         if (error.response.status == 400) {
                             this.error_sms_code_message = error.response.data.message;
                             this.error_sms_code = true;
