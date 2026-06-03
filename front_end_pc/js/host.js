8c99262746c0f2ac0bc86e861c9d4a19058bc83a
ccy <2074441723@qq.com>
2026-06-02 20:17:07 -0400

    updated front end to support https

--- front_end_pc/js/host.js
+++ front_end_pc/js/host.js
@@ -1,4 +1,5 @@
-// Store the backend API server address
-
-var host = window.location.protocol + '//' + window.location.hostname + ':8000';
-//var host = 'http://127.0.0.1:8000';
+// Store the backend API server address.
+// HTTPS pages should use the nginx proxy on the same origin.
+var host = window.location.protocol === 'https:'
+    ? window.location.origin
+    : 'http://' + window.location.hostname + ':8000';
