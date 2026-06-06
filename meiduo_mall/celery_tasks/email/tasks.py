3804a76fd4f32f04aba542125dc672d17dcbd9b3
ccy <2074441723@qq.com>
2026-06-06 10:47:24 -0400

    updated annotation

--- meiduo_mall/celery_tasks/email/tasks.py
+++ meiduo_mall/celery_tasks/email/tasks.py
@@ -3,6 +3,22 @@ from celery_tasks.main import app
 
 @app.task
 def celery_send_email(subject, message, from_email, recipient_list, html_message):
+  """
+      Uses celery to handle sending email.
+      Args:
+          subject:
+            Email subject.
+          message:
+            Email message.
+          from_email:
+            Sender.
+          recipient_list:
+            Send email to recipients.
+          html_message:
+            Sends html message.(imgs .etc)
+      Returns:
+          Returns 0 on success.
+  """
   send_mail(
     subject=subject,
     message=message,
--- meiduo_mall/celery_tasks/sms/tasks.py
+++ meiduo_mall/celery_tasks/sms/tasks.py
@@ -3,4 +3,14 @@ from celery_tasks.main import app
 
 @app.task
 def celery_send_sms_code(mobile,code):
+  """
+      Uses celery to handle sending SMS message.
+      Args:
+          mobile:
+            User provided mobile.
+          code:
+            System generated code.
+      Returns:
+          Returns 0 on success, -1 otherwise.
+  """
   CCP().send_template_sms(mobile,[code,5],1)
