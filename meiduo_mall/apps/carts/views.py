7f4d483bddb436aa08c3e3492717a1ada69a2ba7
ccy <2074441723@qq.com>
2026-06-08 11:13:51 -0400

    updated annotation

--- meiduo_mall/apps/carts/views.py
+++ meiduo_mall/apps/carts/views.py
@@ -2,15 +2,28 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpRequest, JsonResponse, HttpResponse
 # Create your views here.
+import utils.responses.general_response as res
 
 import json
 from apps.goods.models import SKU
 from django_redis import get_redis_connection
 import pickle
 import base64
+import meiduo_mall.errors as error
 class CartsView(View):
   @staticmethod
   def _normalize_carts(carts):
+    """
+    Organizes cart data.
+    Args:
+        cart:
+            everything inside cart.
+
+    Returns:
+        normalized_carts:
+          organized cart.
+    """
+
     normalized_carts = {}
     for sku_id, cart in carts.items():
       try:
@@ -26,6 +39,17 @@ class CartsView(View):
     return normalized_carts
 
   def get(self, request:HttpRequest):
+    """
+    Gets cart info.
+    Args:
+        request:
+            request from frontend.
+
+    Returns:
+        Json response:
+          JsonResponse with organized cart data.
+    """
+
     user = request.user
     if user.is_authenticated:
       redis_cli = get_redis_connection('carts')
@@ -60,8 +84,19 @@ class CartsView(View):
         'count':carts[sku.id]['count'],
         'amount':sku.price * carts[sku.id]['count']
       })
-    return JsonResponse({'code':0, 'errmsg':'ok','cart_skus':sku_list})
+    return JsonResponse({'code':0, 'errmsg':error.NO_ERROR,'cart_skus':sku_list})
+  
   def post(self, request:HttpRequest):
+    """
+    Allows user to add items to their carts.
+    Args:
+        request:
+            request from frontend.
+
+    Returns:
+        Json response:
+          JsonResponse with OK on success.
+    """
     data = json.loads(request.body.decode())
     sku_id = data.get('sku_id')
     count = data.get('count')
@@ -69,7 +104,7 @@ class CartsView(View):
     try:
       SKU.objects.get(id=sku_id)
     except SKU.DoesNotExist:
-      return JsonResponse({'code':400, 'errmsg':'No Such Data'})
+      return res.JsonResponseError(errmsg=error.DOES_NOT_EXIST)
     
     try:
       count = int(count)
@@ -81,7 +116,7 @@ class CartsView(View):
       redis_cli = get_redis_connection('carts')
       redis_cli.hincrby('carts_%s'%user.id, sku_id, count)
       redis_cli.sadd('selected_%s'%user.id, sku_id)
-      return JsonResponse({'code':0, 'errmsg':'ok'})
+      return res.JsonResponsePass(errmsg=error.NO_ERROR)
     else:
       cookie_carts = request.COOKIES.get('carts')
       if cookie_carts:
@@ -100,11 +135,21 @@ class CartsView(View):
       }
       carts_bytes = pickle.dumps(carts)
       base64encode = base64.b64encode(carts_bytes)
-      response = JsonResponse({'code':0, 'errmsg':'ok'})
+      response = JsonResponse({'code':0, 'errmsg':error.NO_ERROR})
       response.set_cookie('carts',base64encode.decode(), max_age=3600*24*7)
       return response
 
   def put(self,request:HttpRequest):
+    """
+    Allows user to add/substract items in their cart.
+    Args:
+        request:
+            request from frontend.
+
+    Returns:
+        Json response:
+          JsonResponse with OK on success,400 on failure.
+    """
     user = request.user
     data = json.loads(request.body.decode())
     sku_id = data.get('sku_id')
@@ -112,11 +157,11 @@ class CartsView(View):
     selected = data.get('selected')
 
     if not all([sku_id, count]):
-      return JsonResponse({'code':400,'errmsg':'Bad Params'})
+      return res.JsonResponseError(errmsg=error.INSUFFICIENT_DATA)
     try:
       SKU.objects.get(id=sku_id)
     except SKU.DoesNotExist:
-      return JsonResponse({'code':400,'errmsg':'Bad Params'})
+      return res.JsonResponseError(errmsg=error.INSUFFICIENT_DATA)
     try:
       count = int(count)
     except Exception:
@@ -131,7 +176,7 @@ class CartsView(View):
       else:
         pipeline.srem('selected_%s'%user.id, sku_id)
       pipeline.execute()
-      return JsonResponse({'code':0, 'errmsg':'OK', 'cart_sku':{'count':count, 'selected':selected}})
+      return JsonResponse({'code':0, 'errmsg':error.NO_ERROR, 'cart_sku':{'count':count, 'selected':selected}})
     else:
       cookie_cart = request.COOKIES.get('carts')
       if cookie_cart is not None:
@@ -149,23 +194,33 @@ class CartsView(View):
         }
       
       new_carts = base64.b64encode(pickle.dumps(carts))
-      response = JsonResponse({'code':0, 'errmsg':'OK', 'cart_sku':{'count':count, 'selected':selected}})
+      response = JsonResponse({'code':0, 'errmsg':error.NO_ERROR, 'cart_sku':{'count':count, 'selected':selected}})
       response.set_cookie('carts', new_carts.decode(), max_age=3600*24*7)
       return response
 
   def delete(self, request:HttpRequest):
+    """
+    Allows user to delete items in their cart.
+    Args:
+        request:
+            request from frontend.
+
+    Returns:
+        Json response:
+          JsonResponse with OK on success,400 on failure.
+    """
     data = json.loads(request.body.decode())
     sku_id = data.get('sku_id')
     try:
       SKU.objects.get(pk=sku_id)
     except SKU.DoesNotExist:
-      return JsonResponse({'code':400, 'errmsg':'No such thing'})
+      return res.JsonResponseError(errmsg=error.DOES_NOT_EXIST)
     user = request.user
     if user.is_authenticated:
       redis_cli = get_redis_connection('carts')
       redis_cli.hdel('carts_%s'%user.id, sku_id)
       redis_cli.srem('selected_%s'%user.id, sku_id)
-      return JsonResponse({'code':0, 'errmsg':'OK'})
+      return res.JsonResponsePass(errmsg=error.NO_ERROR)
     else:
       cookie_cart = request.COOKIES.get('carts')
       if(cookie_cart is not None):
@@ -178,12 +233,21 @@ class CartsView(View):
       if sku_id in carts:
         del carts[sku_id]
       new_carts = base64.b64encode(pickle.dumps(carts))
-      response = JsonResponse({'code':0, 'errmsg':'OK'})
+      response = JsonResponse({'code':0, 'errmsg':error.NO_ERROR})
       response.set_cookie('carts', new_carts.decode(), max_age=7*3600*24)
       return response
 
 
 class CartsSelectAllView(View):
+  """
+  Allows user to select all items in their cart.
+  Args:
+      request:
+          request from frontend.
+  Returns:
+      Json response:
+        JsonResponse with OK on success,400 on failure.
+  """
   def put(self, request:HttpRequest):
     data = json.loads(request.body.decode())
     selected = data.get('selected')
@@ -197,7 +261,7 @@ class CartsSelectAllView(View):
           redis_cli.sadd('selected_%s'%user.id, *sku_ids)
         else:
           redis_cli.srem('selected_%s'%user.id, *sku_ids)
-      return JsonResponse({'code':0, 'errmsg':'OK', 'selected':selected})
+      return JsonResponse({'code':0, 'errmsg':error.NO_ERROR,'selected':selected})
 
     cookie_cart = request.COOKIES.get('carts')
     if cookie_cart is not None:
@@ -209,7 +273,7 @@ class CartsSelectAllView(View):
     for sku_id in carts:
       carts[sku_id]['selected'] = selected
 
-    response = JsonResponse({'code':0, 'errmsg':'OK', 'selected':selected})
+    response = JsonResponse({'code':0, 'errmsg':error.NO_ERROR, 'selected':selected})
     response.set_cookie(
       'carts',
       base64.b64encode(pickle.dumps(carts)).decode(),
