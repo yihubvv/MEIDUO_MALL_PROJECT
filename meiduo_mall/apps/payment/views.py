7faac65320a564095691d423d8a05d44cf9d39d0
ccy <2074441723@qq.com>
2026-06-09 13:22:37 -0400

    saves order

--- meiduo_mall/apps/payment/views.py
+++ meiduo_mall/apps/payment/views.py
@@ -6,6 +6,7 @@ import meiduo_mall.settings as settings
 from utils.view import LoginRequiredJsonMixin
 from django.views import View
 from apps.orders.models import OrderInfo
+from apps.payment.models import Payment
 from alipay import AliPay
 # Create your views here.
 class PaymentView(LoginRequiredJsonMixin, View):
@@ -37,3 +38,35 @@ class PaymentView(LoginRequiredJsonMixin, View):
 
         alipay_url = settings.ALIPAY_URL + "?" + order_string
         return JsonResponse({'code': 0, 'errmsg': error.NO_ERROR, 'alipay_url': alipay_url})
+    
+import os
+class PaymentStatusView(View):
+
+    def get(self, request):
+        query_dict = request.GET
+        data = query_dict.dict()
+        signature = data.pop('sign')
+
+        alipay = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,
+            app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys/app_private_key.pem"),
+            alipay_public_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys/alipay_public_key.pem"),
+            sign_type="RSA2",
+            debug=settings.ALIPAY_DEBUG
+        )
+        success = alipay.verify(data, signature)
+        if success:
+            order_id = data.get('out_trade_no')
+            trade_id = data.get('trade_no')
+            Payment.objects.create(
+                order_id=order_id,
+                trade_id=trade_id
+            )
+
+            OrderInfo.objects.filter(order_id=order_id, status=OrderInfo.ORDER_STATUS_ENUM['UNPAID']).update(
+                status=OrderInfo.ORDER_STATUS_ENUM["UNCOMMENT"])
+
+            return JsonResponse({'code': 0, 'errmsg': error.NO_ERROR, 'trade_id': trade_id})
+        else:
+            return JsonResponseError(errmsg=error.FAILED_UPDATE)
