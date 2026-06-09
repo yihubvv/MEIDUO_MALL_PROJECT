c845e1480bfad94678fd3e35540588535d9baceb
ccy <2074441723@qq.com>
2026-06-09 13:23:21 -0400

    saves order

--- meiduo_mall/apps/payment/urls.py
+++ meiduo_mall/apps/payment/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.payment.views import PaymentView
+from apps.payment.views import PaymentView,PaymentStatusView
 urlpatterns = [
     path('payment/<order_id>/', PaymentView.as_view()),
+    path('payment/status/', PaymentStatusView.as_view()),
 
 ]
